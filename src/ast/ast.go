7054296df6706386d075a28cb7e3471bfac85030
Shashank Jarmale <shashankjarmale1@gmail.com>
2024-10-18 20:29:19 -0400

    Added basic implementation of AST types & functionality prior to implementing parser

---
+++ src/ast/ast.go
@@ -0,0 +1,59 @@
+package ast
+
+import "monkey/token"
+
+// Represents a Node in the AST.
+type Node interface {
+	TokenLiteral() string
+}
+
+// Represents a single statement in the series included in the Monkey program.
+type Statement interface {
+	Node
+	statementNode()
+}
+
+// Represents an expression (produces a value) included in the Monkey program.
+type Expression interface {
+	Node
+	expressionNode()
+}
+
+// Represents the root node of every Monkey AST.
+type Program struct {
+	Statements []Statement
+}
+
+func (p *Program) TokenLiteral() string {
+	if len(p.Statements) > 0 {
+		return p.Statements[0].TokenLiteral()
+	} else {
+		return ""
+	}
+}
+
+// Represents a let statement in the Monkey programming language, consisting of (1) the LET token, (2) the
+// name of the identifier in the binding, and (3) the expression that produces the value for the binding.
+type LetStatement struct {
+	Token token.Token // the token.LET token
+	Name  *Identifier
+	Value Expression
+}
+
+func (ls *LetStatement) statementNode() {}
+
+func (ls *LetStatement) TokenLiteral() string {
+	return ls.Token.Literal
+}
+
+// Represents an identifier, consisting of the IDENT token and the value (name of the identifier).
+type Identifier struct {
+	Token token.Token // the token.IDENT token
+	Value string
+}
+
+func (i *Identifier) expressionNode() {}
+
+func (i *Identifier) TokenLiteral() string {
+	return i.Token.Literal
+}
