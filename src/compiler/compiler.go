013642ee56d8a1ff9a3eea223be4911445d5461b
Shashank Jarmale <shashankjarmale1@gmail.com>
2024-11-05 12:20:55 -0500

    Add main compilation function body to compiler package

--- src/compiler/compiler.go
+++ src/compiler/compiler.go
@@ -26,6 +26,37 @@ func NewCompiler() *Compiler {
 }
 
 func (c *Compiler) Compile(node ast.Node) error {
+	switch node := node.(type) {
+	case *ast.Program:
+		for _, s := range node.Statements {
+			err := c.Compile(s)
+			if err != nil {
+				return err
+			}
+		}
+
+	case *ast.ExpressionStatement:
+		err := c.Compile(node.Expression)
+		if err != nil {
+			return err
+		}
+
+	case *ast.InfixExpression:
+		err := c.Compile(node.Left)
+		if err != nil {
+			return err
+		}
+
+		err = c.Compile(node.Right)
+		if err != nil {
+			return err
+		}
+
+	case *ast.IntegerLiteral:
+		integer := &object.Integer{Value: node.Value}
+		c.emit(bytecode.OpConstant, c.addConstant(integer))
+	}
+
 	return nil
 }
 
@@ -34,4 +65,20 @@ func (c *Compiler) Bytecode() *Bytecode {
 		Instructions: c.instructions,
 		Constants:    c.constants,
 	}
+}
+
+func (c *Compiler) emit(op bytecode.Opcode, operands ...int) int {
+	instr := bytecode.Make(op, operands...)
+	pos := c.addInstruction(instr)
+	return pos
+}
+
+func (c *Compiler) addConstant(obj object.Object) int {
+	c.constants = append(c.constants, obj)
+	return len(c.constants) - 1
+}
+
+func (c *Compiler) addInstruction(instr []byte) int {
+	c.instructions = append(c.instructions, instr...)
+	return len(c.instructions) - 1
 }
